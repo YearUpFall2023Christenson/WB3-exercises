c0afae338add28226ad24ed92cf962f2188cdff0
mattchristenson <matt@rcc.team>
2023-10-24 12:16:49 -0500

    finished ex 1-12

---
+++ demos/demo1.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function doSomething(){
+    //some code goes here.
+    console.log("something")
+}
+
+
+function doSomethingElse(){
+    console.log("else");
+}
+
+doSomething();
+doSomething();
+doSomethingElse();
+
+
